04b7312e776573c60c866f9449a89644c7a229b3
parkcrystal <9jeworld@naver.com>
2020-05-23 16:16:39 +0900

    확인버튼 누르면 => 로그로 눌렸다고 출력

--- app/src/main/java/com/phis/kotlinbasic_20200523/MainActivity.kt
+++ app/src/main/java/com/phis/kotlinbasic_20200523/MainActivity.kt
@@ -2,11 +2,17 @@ package com.phis.kotlinbasic_20200523
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        okBtn.setOnClickListener { 
+            Log.d("메인화면", "확인버튼 눌림")
+        }
     }
 }
